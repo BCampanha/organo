e69c0911b1aeb4547a31bccea281aa01b1891d68
Beatriz Campanha <bcampanha@usp.br>
2024-01-18 11:17:05 -0300

    mapeando os colaboradores e seus cards

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,15 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+// const Colaborador = (props) => {
+const Colaborador = ({ nome, imagem, cargo }) => {
   return(
     <div className='colaborador'>
       <div className='cabecalho'>
-        <img src='https://github.com/BCampanha.png' alt=''/>
+        <img src={imagem} alt=''/>
       </div>
       <div className='rodape'>
-        <h4>Beatriz Campanha</h4>
-        <h5>Estag</h5>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
       </div>
     </div>
   )
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,16 +1,22 @@
-import Colaborador from '../Colaborador'
 import './Time.css'
+import Colaborador from './../Colaborador'
 
 const Time = (props) => {
   const cssTime = {backgroundColor: props.corSecundaria}
   const cssTimeH3 = {borderColor: props.corPrimaria}
-  
+
   return(
     <section className='time' style={cssTime}>
       <h3 style={cssTimeH3} >{props.nome}</h3>
-      <Colaborador />
-      <Colaborador />
-      <Colaborador />
+      <div className='colaboradores'>
+        {props.colaboradores.map(colaborador =>
+          <Colaborador
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+          />)}
+      </div>
     </section>
   )
 }
