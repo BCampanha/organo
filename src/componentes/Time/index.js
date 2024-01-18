b83925f201c0c475268140b14e01ec7f79cd6e21
Beatriz Campanha <bcampanha@usp.br>
2024-01-18 10:33:23 -0300

    criando o componente time e estilizando

---
+++ src/componentes/Time/index.js
@@ -0,0 +1,14 @@
+import './Time.css'
+
+const Time = (props) => {
+  const cssTime = {backgroundColor: props.corSecundaria}
+  const cssTimeH3 = {borderColor: props.corPrimaria}
+  
+  return(
+    <section className='time' style={cssTime}>
+      <h3 style={cssTimeH3} >{props.nome}</h3>
+    </section>
+  )
+}
+
+export default Time
